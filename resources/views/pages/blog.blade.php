4e470d1af6a55f3b3f918d2e101f0edc048c3d03
ankurg132 <ankurg052@gmail.com>
2021-07-09 12:08:45 +0530

    Bug fix

--- resources/views/pages/blog.blade.php
+++ resources/views/pages/blog.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
 <style>
-  .blogcontent * 
+  .blogcontent *{
     color:#fffa !important;
   }
 </style>
@@ -33,7 +33,7 @@
     </div>
   </div>
 
-<div style="blogcontent background: var(--darkestShade);color:#fffa;font-size:1.2em;word-spacing:0.2em;">
+<div class="blogcontent" style="background: var(--darkestShade);color:#fffa;font-size:1.2em;word-spacing:0.2em;">
   <div class=" row mx-3 py-5">
   <div class="my-3 px-5 col-md-8" style="text-align: justify;">
   {!!$blog->content!!}
