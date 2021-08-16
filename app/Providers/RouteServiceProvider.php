297b7c6316c6c216897cb592cde6fedf937a9e6d
meinhoonharsh <harshprogrammer782@gmail.com>
2021-08-17 00:56:54 +0530

    Home Route Changed

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/dash';
 
     /**
      * The controller namespace for the application.
