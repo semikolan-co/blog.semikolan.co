e1c5a337451d63bf66d598f95d3c26ff08f13a2f
meinhoonharsh <harshprogrammer782@gmail.com>
2021-08-18 02:33:18 +0530

    GitGuardian Issue Solved

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,7 +2,6 @@
     
    <!-- Bootstrap  5.0 CSS -->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-
    <script src="https://kit.fontawesome.com/c5fe5e7547.js" crossorigin="anonymous"></script>
  
  
