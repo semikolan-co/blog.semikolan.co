236472866f17a421bc989625c2b5a1321128f58e
meinhoonharsh <harshprogrammer782@gmail.com>
2021-09-14 00:27:36 +0530

    Created Migration for blog_subcategories Table

--- database/migrations/2021_09_13_183508_create_blog_subcategories_table.php
+++ database/migrations/2021_09_13_183508_create_blog_subcategories_table.php
@@ -13,8 +13,19 @@ class CreateBlogSubcategoriesTable extends Migration
      */
     public function up()
     {
+        // Schema
+        // id	int(11) Auto Increment	
+        // sname	varchar(50)	NOT NULL
+        // parent_category	int(11)	
+        // active	int(11) [1]	
+        // created_at	datetime	
+        // updated_at	datetime
         Schema::create('blog_subcategories', function (Blueprint $table) {
             $table->id();
+            $table->primary('id');
+            $table->string('sname', 50)->nullable();
+            $table->integer('parent_category')->unsigned();
+            $table->boolean('active')->default(1);
             $table->timestamps();
         });
     }
