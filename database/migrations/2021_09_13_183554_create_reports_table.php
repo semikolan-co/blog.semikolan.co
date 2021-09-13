7b96ad8d8fca55b30f57f872782c2f02577d3ff3
meinhoonharsh <harshprogrammer782@gmail.com>
2021-09-14 00:41:00 +0530

    Added Migration for reports Table

--- database/migrations/2021_09_13_183554_create_reports_table.php
+++ database/migrations/2021_09_13_183554_create_reports_table.php
@@ -15,6 +15,10 @@ class CreateReportsTable extends Migration
     {
         Schema::create('reports', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('report');
+            $table->boolean('action_taken')->nullable();
             $table->timestamps();
         });
     }
